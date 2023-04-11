b9eb858c928d9e4cf5ff90985ae921841d7eca02
João Pedro Padilha <jppadilha11@gmail.com>
2023-04-11 10:25:02 -0300

    new-updates

--- script/script.js
+++ script/script.js
@@ -1,12 +1,27 @@
-const p1 = youngerBrother();
-const p2 = middleBrother();
+const p1 = 0;
+const p2 = 0;
 
-stage.start(
-    p1,
-    p2,
-    document.querySelector("#brother1"),
-    document.querySelector("#brother2")
-);
+/*let older = document.querySelector('.select').addEventListener('click', () => getPlayers());
+let middle = document.querySelector('#middle').addEventListener('click', () => getPlayers("middle"));
+
+
+function getPlayers(Player1,Player2) {
+    console.log(`${Player1} ${Player2}`);
+}*/
+
+function start() {
+    document.querySelector('.reload').style.display = 'inline';
+    document.querySelector('.start').style.display = 'none';
+    document.querySelector('.content').style.display = 'flex';
+    document.querySelector('.menu').style.display = 'none';
+
+    stage.start(
+        p1,
+        p2,
+        document.querySelector("#brother1"),
+        document.querySelector("#brother2")
+    );
+}
 
 function reload() {
     window.location.reload();
