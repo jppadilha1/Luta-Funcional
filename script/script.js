cc9c98cf1951d557d7b722067623d333fac5d3fe
João Pedro Padilha <jppadilha11@gmail.com>
2023-04-12 10:08:12 -0300

    build

--- script/script.js
+++ script/script.js
@@ -1,13 +1,22 @@
 const p1 = 0;
 const p2 = 0;
 
-/*let older = document.querySelector('.select').addEventListener('click', () => getPlayers());
-let middle = document.querySelector('#middle').addEventListener('click', () => getPlayers("middle"));
+let select1 = document.querySelector('.select1');
+let select2 = document.querySelector('.select2');
+select1.querySelector('#older').addEventListener('click', () => getPlayers("older"));
+select2.querySelector('#older').addEventListener('click', getPlayers("older"));
 
 
-function getPlayers(Player1,Player2) {
-    console.log(`${Player1} ${Player2}`);
-}*/
+function getPlayers(Player) {
+    return console.log(Player);
+    /*let card1 = document.querySelector('.card1 img');
+    let card2 = document.querySelector('.card2 img');
+    if (Player == "older") {
+        return p1 = olderBrother();
+    }*/
+    
+    
+}
 
 function start() {
     document.querySelector('.reload').style.display = 'inline';
